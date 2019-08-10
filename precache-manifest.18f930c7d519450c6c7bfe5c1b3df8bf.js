32c96f814cba81b5d3cf1391bd61d505d2aae1bf
kristapsv <kristaps.vitols@printful.com>
2019-08-10 14:22:48 +0300

    Updates

---
+++ precache-manifest.18f930c7d519450c6c7bfe5c1b3df8bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903f9545b8ae8dc699ffb8164e1e9a3d",
+    "url": "/rolodex-react/index.html"
+  },
+  {
+    "revision": "5e1d5e899d9870d861d4",
+    "url": "/rolodex-react/static/css/main.b2b56960.chunk.css"
+  },
+  {
+    "revision": "eb0ca0d4668f14c5b556",
+    "url": "/rolodex-react/static/js/2.e5a9de09.chunk.js"
+  },
+  {
+    "revision": "5e1d5e899d9870d861d4",
+    "url": "/rolodex-react/static/js/main.eb2177b4.chunk.js"
+  },
+  {
+    "revision": "d67c6c7e6d0f94b12ad5",
+    "url": "/rolodex-react/static/js/runtime~main.ccc7cab1.js"
+  }
+]);
